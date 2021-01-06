c52555d09bf547a7df71b04cf01efaafae1cbc28
Dobromił <db.cepil@gmail.com>
2021-01-06 18:16:14 +0100

    add char limit to post input box, remove unnecessary import from searchbar component

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import InputBase from "@material-ui/core/InputBase";
-import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
