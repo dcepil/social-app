9e3f7f840e7c73141eaa8a60c4c2334709c60f7b
Dobromił <db.cepil@gmail.com>
2021-01-12 14:17:44 +0100

    initial api generation and database connection setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.MONGO_ATLAS_CLUSTER}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    ),
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}
